149fe91e1ae12558e23b5457920c891e60575d31
Sleggi <soltankirill23@gmail.com>
2020-09-02 12:59:23 +0300

    Данные с мок сервера db.json. Aios, prop-types

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import { Header } from './components'
 import { Home, Cart } from './pages'
 import { Route } from 'react-router-dom';
@@ -7,15 +8,27 @@ import { Route } from 'react-router-dom';
 
 
 
+
 function App() {
 
+  const [pizzas, setPizzas] = useState([])
+
+  console.log(pizzas)
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/db.json").then((resp) => { setPizzas(resp.data.pizzas) })
+    // // Чтобы иметь доступ к файлам по ссылке, закидываем их в папку public
+    // fetch('http://localhost:3000/db.json')
+    //   .then((resp) => resp.json())
+    //   .then((json) => { setPizzas(json.pizzas) })
+  }, [])
 
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
         <div className="content">
-          <Route path='/' component={Home} exact />
+          <Route path='/' render={() => <Home items={pizzas} />} exact />
           <Route path='/cart' component={Cart} exact />
         </div>
       </div>
